0356c451b7801634a02a001189fbcf4490d4f329
unknown <qa.farheen@gmail.com>
2023-04-08 10:00:35 -0400

    Hello world

---
+++ src/CalculatorTopic.java
@@ -0,0 +1,12 @@
+public class CalculatorTopic {
+    public static void main(String[] args) {
+
+    }
+
+    int b= 125;
+    int c = (a/b) ;
+
+
+
+
+}
---
+++ src/com/syntexpro/qa/VariablesTopic.java
@@ -0,0 +1,18 @@
+package com.syntexpro.qa;
+
+public class VariablesTopic {
+  public static void main(String[] args) {
+   int a = 10;
+      int b = 5;
+
+       a = 25;
+      b = 8;
+   System.out.println(a);
+     System.out.println(b);
+   int studentId =2;
+   studentId = 10;
+  System.out.print(studentId);
+   }
+
+
+}
