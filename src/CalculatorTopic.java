b2f7d9fa28148202e218dbe191cb56b4fbb99624
unknown <qa.farheen@gmail.com>
2023-04-08 10:08:17 -0400

    adding

--- src/CalculatorTopic.java
+++ src/CalculatorTopic.java
@@ -1,10 +1,11 @@
 public class CalculatorTopic {
     public static void main(String[] args) {
-
+        int b= 125;
+        int a = 25;
+        int c = (a/b) ;
     }
 
-    int b= 125;
-    int c = (a/b) ;
+  
 
 
 
